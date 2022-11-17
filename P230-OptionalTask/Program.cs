3e99b6217ce09121f1f779714b6871eb498d89ff
unknown <you@example.com>
2022-11-17 11:40:59 +0400

    Optional Task

---
+++ P230-OptionalTask/Program.cs
@@ -0,0 +1,36 @@
+﻿#region Ededin reqeqmlerinin cemi
+int num = 123;
+int result = 0;
+
+while (num > 0)
+{
+    result += (num % 10);
+    num /= 10;
+}
+//Console.WriteLine(result);
+#endregion
+
+
+#region 7 e en yaxin bolunen
+//int closest_number = 0;
+
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//int remainder = number % 7;
+
+
+//if (remainder == 0)
+//{
+//    closest_number = number;
+//}
+//else if (remainder <= 3)
+//{
+//    closest_number = number - remainder;
+//}
+//else if (remainder > 3)
+//{
+//    closest_number = number + 7 - remainder;
+//};
+//Console.Write("En yaxin bolunen eded :");
+//Console.WriteLine(closest_number); 
+#endregion
